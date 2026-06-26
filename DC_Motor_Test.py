7ba087963dc166f20950aa030ec16da46741a30c
MohamedAshiqBasha <98243585+MohamedAshiqBasha@users.noreply.github.com>
2026-06-25 19:40:40 -0500

    Update DC_Motor_Test.py

--- DC_Motor_Test.py
+++ DC_Motor_Test.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
-ENA = 18
-IN1 = 23
-IN2 = 24
+ENA = 12   # GPIO12 -> L293D pin 1
+IN1 = 5    # GPIO5  -> L293D pin 2
+IN2 = 6    # GPIO6  -> L293D pin 7
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -12,27 +12,29 @@ GPIO.setup(ENA, GPIO.OUT)
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
 
-GPIO.output(ENA, GPIO.HIGH)
-
 try:
-    print("FORWARD TEST: IN1 HIGH, IN2 LOW")
+    print("Enable motor")
+    GPIO.output(ENA, GPIO.HIGH)
+
+    print("Forward: IN1 HIGH, IN2 LOW")
     GPIO.output(IN1, GPIO.HIGH)
     GPIO.output(IN2, GPIO.LOW)
-    time.sleep(8)
+    time.sleep(5)
 
-    print("STOP")
+    print("Stop")
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.LOW)
-    time.sleep(3)
+    time.sleep(2)
 
-    print("REVERSE TEST: IN1 LOW, IN2 HIGH")
+    print("Reverse: IN1 LOW, IN2 HIGH")
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.HIGH)
-    time.sleep(8)
+    time.sleep(5)
 
-    print("STOP")
+    print("Stop")
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(ENA, GPIO.LOW)
 
 finally:
     GPIO.cleanup()
